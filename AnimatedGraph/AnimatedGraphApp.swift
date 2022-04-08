54bd537a5a6c8d4d6c77265583bc967971b5119e
DanDolog <pyatp@bk.ru>
2022-04-08 16:28:00 +0300

    Initial Commit

---
+++ AnimatedGraph/AnimatedGraphApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimatedGraphApp.swift
+//  AnimatedGraph
+//
+//  Created by Dan Dolog on 08.04.2022.
+//
+
+import SwiftUI
+
+@main
+struct AnimatedGraphApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
